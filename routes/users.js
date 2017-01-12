84be34645bfcec32205caa2a45f87cbef9801573
codejianqxr <1046396106@qq.com>
2017-01-12 11:48:43 +0800

    mv

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ var upload = require('./fileuploads');
 var unzip=require("unzip");
 var fs=require("fs");
 var async=require("async");
+var shell=require('shelljs/global');
+
+var compUrl=process.cwd()+"/comp";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -19,16 +22,30 @@ router.post('/upload', upload.single('file'), function (req, res, next) {
                 setTimeout(function () {
                     cb();
                 },2000)
-
             },
             //读取json文件并创建目录结构
             function(cb){
+              var rmdir=compUrl+"/__MACOSX";
+
+              try {
+                  var info=fs.statSync(rmdir);
+                  if(info){
+
+                      rm('-rf', rmdir);
+                  }
+              }catch (e){
+
+              }
+
               var packageUrl=process.cwd()+"/comp/package.json";
               var packageData=JSON.parse(fs.readFileSync(packageUrl).toString());
               var category=packageData.category;
               var name=packageData.name;
-             
-
+              //根据json文件的信息，来创建相应的目录
+              var destdir=  compUrl+"/"+category+"/"+name
+              mkdir('-p',destdir );
+              //将文件移动到指定的目录里面
+              mv(compUrl+"/code",compUrl+"/layout",destdir);
               cb();
             }
 
