e8a63ae0449112e158e215fa96fa8bef2f344a72
codejianqxr <1046396106@qq.com>
2017-01-12 10:03:54 +0800

    upload

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var upload = require('./fileuploads');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render("admin");
 });
+router.post('/upload', upload.single('file'), function (req, res, next) {
+    if (req.file) {
+        res.send('文件上传成功')
+        console.log(req.file);
+        console.log(req.body);
+    }
+});
+
 
 module.exports = router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React,{Component} from "react";
 import ReactDOM,{render} from "react-dom";
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
 class Main extends Component{
     constructor(){
         super();
     }
+
     render(){
         return(
-            <div>首页</div>
+            <div>
+                <DatePicker />
+            </div>
+
         )
     }
 }
