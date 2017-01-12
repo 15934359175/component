de2f22b305b2d162fe25152b9e5be8e3726fc474
codejianqxr <1046396106@qq.com>
2017-01-12 16:57:37 +0800

    ajax

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 var express = require('express');
 var router = express.Router();
+var mysql=require("mysql");
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'component'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/comp/:cat/:com/layout/index.html', function(req, res, next) {
+    var caturl=req.params.cat;
+    var comurl=req.params.com;
+    var url=process.cwd()+"/comp/"+caturl+"/"+comurl+"/layout/index.html";
+
+    res.sendFile(url);
+});
+router.get('/comp/:cat/:com/:zip', function(req, res, next) {
+    var caturl=req.params.cat;
+    var comurl=req.params.com;
+    var zip=req.params.zip;
+    var url=process.cwd()+"/comp/"+caturl+"/"+comurl+"/"+zip;
+    res.sendFile(url);
+});
+
+router.get("/ajaxUrl",function(req,res){
+      db.query("select * from component",function(error,rows){
+          if(error){
+              console.log(error);
+          }else{
+              if(rows.length>0){
+
+                  var arr=[];
+
+                  for(var i=0;i<rows.length;i++){
+                      if(rows[i].pid==0){
+                          var obj={catname:rows[i].catname,id:rows[i].id};
+                          arr.push(obj);
+                      }
+                  }
+
+                  var result={};
+
+                  for(var i=0;i<arr.length;i++){
+                      var newarr=[];
+                      for(var j=0;j<rows.length;j++){
+                          if(arr[i].id==rows[j].pid){
+                              var obj={
+                                  catname:rows[j].catname,
+                                  url:rows[j].url,
+                                  zurl:rows[j].zurl
+                              }
+                             newarr.push(obj)
+                          }
+                      }
+                      result[arr[i].catname]=newarr;
+                  }
+
+                  res.send(JSON.stringify(result));
+
+              }else{
+                  res.send(JSON.stringify(['出错了']));
+              }
+          }
+      })
+})
+
 
 module.exports = router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,79 @@
 import React,{Component} from "react";
 import ReactDOM,{render} from "react-dom";
-import { DatePicker } from 'antd';
 import 'antd/dist/antd.css';
-
-
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
 class Main extends Component{
     constructor(){
         super();
+        this.state={data:{}};
     }
-
+    componentDidMount(){
+        var that=this;
+        var url=this.props.ajaxUrl;
+        var xmlobj=new XMLHttpRequest();
+        xmlobj.onload=function(){
+               var data=JSON.parse(xmlobj.response);
+               console.log(data);
+               that.setState({data:data});
+        }
+        xmlobj.open("get",url);
+        xmlobj.send();
+    }
     render(){
-        return(
-            <div>
-            <DatePicker/>
-            </div>
+        return (
+            <Layout>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '12px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                        <Sider width={200}>
+                            <Menu
+                                mode="inline"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                            >
+                                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                                    <Menu.Item key="1">option1</Menu.Item>
+                                    <Menu.Item key="2">option2</Menu.Item>
+                                    <Menu.Item key="3">option3</Menu.Item>
+                                    <Menu.Item key="4">option4</Menu.Item>
+                                </SubMenu>
+
+
+                            </Menu>
+                        </Sider>
+                        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                            Content
+                        </Content>
+                    </Layout>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©2016 Created by Ant UED
+                </Footer>
+            </Layout>
 
         )
     }
 }
-render(<Main data={["aa","bb"]}/>,document.querySelector("#root"));
+render(<Main ajaxUrl="/ajaxUrl"/>,document.querySelector("#root"))
 
 
 
